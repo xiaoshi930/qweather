a1b386f6c972ee90ae4a75bae74f5de1c29f80b4
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2026-06-01 21:24:30 +0800

    Add files via upload

--- custom_components/qweather/__init__.py
+++ custom_components/qweather/__init__.py
@@ -11,19 +11,7 @@ from homeassistant.helpers.dispatcher import async_dispatcher_send
 from homeassistant.helpers.entity_registry import async_get as get_entity_registry
 from .const import DOMAIN, PLATFORMS
 from homeassistant.const import CONF_HOST, CONF_API_KEY, CONF_NAME, ATTR_ENTITY_ID
-from homeassistant.components.frontend import add_extra_js_url
 from .services import async_setup_services, async_unload_services
-try:
-    from homeassistant.components.http.static import StaticPathConfig
-except ImportError:
-    try:
-        from homeassistant.components.http import StaticPathConfig
-    except ImportError:
-        class StaticPathConfig:
-            def __init__(self, url_path, path, cache_headers):
-                self.url_path = url_path
-                self.path = path
-                self.cache_headers = cache_headers
 _LOGGER = logging.getLogger(__name__)
 # 设置日志级别为INFO，确保删除日志能显示
 _LOGGER.setLevel(logging.INFO)
@@ -32,18 +20,9 @@ CONFIG_SCHEMA = cv.deprecated(DOMAIN)
 # 定义信号常量
 QWEATHER_UPDATE_SIGNAL = f"{DOMAIN}_update"
 
-async def async_setup_weather_card(hass: HomeAssistant) -> bool:
-    state_weather_card_path = '/qweather'
-    await hass.http.async_register_static_paths([
-        StaticPathConfig(state_weather_card_path, hass.config.path('custom_components/qweather/www'), False)
-    ])
-    add_extra_js_url(hass, state_weather_card_path + f"/weather-card.js")
-    return True
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """设置从配置项加载的QWeather组件。"""
     hass.data.setdefault(DOMAIN, {})
-    await async_setup_weather_card(hass)
     await async_setup_services(hass)
 
     # 转发配置到平台
